5c1b090e2d04cc5d0ef57b002870599ceac4f4b6
Mubashir A Abbasi <mubashir@sofdigital.com>
2020-10-07 18:49:45 +0500

    Book request approval work (in progress)

--- booksmanagement/Controllers/api/BookBorrowController.cs
+++ booksmanagement/Controllers/api/BookBorrowController.cs
@@ -1,7 +1,9 @@
 ﻿using booksmanagement.Dtos;
+using booksmanagement.Helpers;
 using booksmanagement.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,6 +16,65 @@ namespace booksmanagement.Controllers.api
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
+        [HttpGet]
+        public async Task<IHttpActionResult> GetPendingRequests()
+        {
+            var appUser = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+            List<BookBorrowOrder> requests = null;
+
+            if (User.IsInRole(RoleName.Admin))
+            {
+                requests = await db.BookBorrowOrders
+                .Include(b => b.Book)
+                .Include(b => b.Applicant)
+                .Where(b => b.Status == 1).OrderBy(b => b.Status).ToListAsync();
+            }
+            else
+            {
+                requests = await db.BookBorrowOrders
+                .Include(b => b.Book)
+                .Where(b => b.ApplicantId == appUser.Id && b.Status == 1).ToListAsync();
+            }
+
+            return Ok(requests);
+        }
+
+        //[HttpGet]
+        //public async Task<IHttpActionResult> GetMyAllRequests()
+        //{
+        //    var appUser = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+
+
+
+        //    return Ok(requests);
+        //}
+
+        [HttpGet]
+        [Authorize(Roles = RoleName.Admin)]
+        public async Task<IHttpActionResult> GetOtherPendingRequests()
+        {
+            var appUser = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+
+            var requests = await db.BookBorrowOrders
+                .Include(b => b.Book)
+                .Where(b => b.Status == 1).ToListAsync();
+
+            return Ok(requests);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = RoleName.Admin)]
+        public async Task<IHttpActionResult> GetOtherAllRequests()
+        {
+            var appUser = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+
+            var requests = await db.BookBorrowOrders
+                .Include(b => b.Book)
+                .OrderBy(b => b.Status).ToListAsync();
+
+            return Ok(requests);
+        }
+
         [HttpPost]
         public async Task<IHttpActionResult> SaveBookRequest(BookRequestDto bookRequest)
         {
@@ -22,7 +83,7 @@ namespace booksmanagement.Controllers.api
 
             var appUser = db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
             var book = db.Books.Where(b => b.Id == bookRequest.BookId).FirstOrDefault();
-            if(book == null)
+            if (book == null)
                 return BadRequest();
 
             if (book.Quantity == null || book.Quantity <= 0 || !book.IsActive)
---
+++ booksmanagement/Scripts/js/ng-controllers/home-ctrl.js
@@ -0,0 +1,11 @@
+﻿myApp.controller('HomeCtrl', function ($scope, $filter, $http, $uibModal, toaster, $ngConfirm) {
+
+    $scope.getPendingRequests = function () {
+        $http.get(root + 'api/BookBorrow/GetPendingRequests').then(function success(response) {
+            $scope.pendingRequests = response.data;
+            console.log('requests:', $scope.pendingRequests);
+        }, function error() { });
+    }
+
+    $scope.getPendingRequests();
+});
